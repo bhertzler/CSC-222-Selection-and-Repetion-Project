8417d43efb465e889c0ec951796aeee7eec02bf6
Ben Hertzler <bwhertzler@outlook.com>
2024-10-10 19:47:13 -0400

    Add project files.

---
+++ CSC 222 Selection and Repetion Project.cpp
@@ -0,0 +1,14 @@
+// CSC 222 Selection and Repetion Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+// Benjamin Hertzler
+// Selection and Repetion Programming Project
+// Master
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
