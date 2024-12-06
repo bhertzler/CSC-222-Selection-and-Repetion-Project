9f3a0c6eab3e0445eee86af31a96690605b81b03
Ben Hertzler <bwhertzler@outlook.com>
2024-12-06 02:55:26 -0500

    initial commit

--- CSC 222 Selection and Repetion Project.cpp
+++ CSC 222 Selection and Repetion Project.cpp
@@ -3,12 +3,69 @@
 
 // Benjamin Hertzler
 // Selection and Repetion Programming Project
-// Master
+// Program 1: Calculator
+
+// Description: This program takes two operations and a +, -, *, / operator and outputs the result. Enter q to quit.
+// Required User Input: 2 integers and an operator, or q to quit.
+// Program Output: The result of the desired operation.
+// Supplied Constants: The 4 available operators (+, -, *, /).
 
 #include <iostream>
+#include <exception>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    char input, operation;
+    int x, y;
+
+	cout << "Calculator: Enter 2 integers and a 4 function operator or 'q' to quit." << endl << endl;
+	try
+	{
+		while (true)
+		{
+			cin >> input;
+			if (tolower(input) != 'q')
+			{
+				if (isdigit(input))
+					x = input - '0';
+				else
+					throw domain_error("Invalid Operand");
+			}
+			else
+				break;
+			cin >> operation >> input;
+			if (isdigit(input))
+				y = input - '0';
+			else
+				throw domain_error("Invalid Operand");
+			switch (operation)
+			{
+			case '+':
+				cout << " = " << x + y << endl;
+				break;
+			case '-':
+				cout << " = " << x - y << endl;
+				break;
+			case '*':
+				cout << " = " << x * y << endl;
+				break;
+			case '/':
+				cout << " = " << x / y << endl;
+				break;
+			default:
+				throw domain_error("Invalid Operator");
+				break;
+			}
+		}
+	}
+	catch (const exception& err)
+	{
+		cout << "ERROR: " << err.what() << endl;
+	}
+	catch (...)
+	{
+		cout << "ERROR: Invalid Input" << endl;
+	}
 }
 
